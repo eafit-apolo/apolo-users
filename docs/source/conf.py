01d2672b4f30c489038e63ec8077348d2c9470c8
Tomás Felipe Llano Ríos <tllano11@gmail.com>
2018-08-22 13:37:29 -0500

    Adding Apolo's logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -97,13 +97,20 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
+html_theme_options = {
+    'logo_only': True,
+}
 # html_theme_options = {}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ['_static']
-html_static_path = []
+html_static_path = ['_static']
+#html_static_path = []
+
+# The name of an image file (relative to this directory) to place at the top
+# of the sidebar.
+html_logo = '_static/apolo-white.png'
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
